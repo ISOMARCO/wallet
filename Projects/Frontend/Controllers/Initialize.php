6f798e571ddda64dd389d7b5409e404e27aeb960
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-11 22:23:29 +0400

    1.0

--- Projects/Frontend/Controllers/Initialize.php
+++ Projects/Frontend/Controllers/Initialize.php
@@ -5,6 +5,7 @@ class Initialize extends Controller
     public function main()
     {
         ML::lang("az");
+        exit("okey");
         if(!Session::id() && CURRENT_CONTROLLER != 'login' && CURRENT_CONTROLLER != 'lang') 
         {
             redirect(URL::base("login"));
