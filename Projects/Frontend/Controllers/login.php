e6f57f90e9c1a70247087a56c5a7e3253c4c262d
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-12 00:16:04 +0400

    1.0

--- Projects/Frontend/Controllers/login.php
+++ Projects/Frontend/Controllers/login.php
@@ -1,10 +1,10 @@
 <?php namespace Project\Controllers;
-use DBTool,ML,MigrateUsers;
+use DBTool,ML;
 class login extends Controller
 {
     public function main()
     {
         DBTool::listTables();
-        #Masterpage::title(ML::select('SignIn'));
+        Masterpage::title(ML::select('SignIn'));
     }
 }
