5a1b3b35e29a6b707f710034f93acc49802c12da
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-11 15:31:41 +0400

    1.0

--- Projects/Frontend/Controllers/login.php
+++ Projects/Frontend/Controllers/login.php
@@ -1,10 +1,10 @@
 <?php namespace Project\Controllers;
-use DB;
+use DB,DBTool;
 class login extends Controller
 {
     public function main()
     {
-        DB::users()->row();
+        output( DBTool::listTables() );
         Masterpage::title("Login");
     }
 }
