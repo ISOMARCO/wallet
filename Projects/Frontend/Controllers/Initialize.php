f54bb0369f5c5edba5f96efb5278eec59e50e886
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-11 10:01:58 +0400

    1.0

--- Projects/Frontend/Controllers/Initialize.php
+++ Projects/Frontend/Controllers/Initialize.php
@@ -8,11 +8,8 @@ class Initialize extends Controller
         {
             redirect(URL::base("login"));
         }
-        # The theme is activated.
-        # Location: Resources/Themes/Default/
         Theme::active('Default');
         
-        # The current settings are being configured.
         Masterpage::headPage('Sections/head')
                   ->bodyPage('Sections/body')
                   ->browserIcon(FILES_DIR . 'favicon.ico');
