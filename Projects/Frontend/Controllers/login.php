62287ddc1eedd8330badb0233f043170506e03ff
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-12 13:08:34 +0400

    1.0

--- Projects/Frontend/Controllers/login.php
+++ Projects/Frontend/Controllers/login.php
@@ -4,6 +4,7 @@ class login extends Controller
 {
     public function main()
     {
+        echo DB::insert('Users',['Name'=>'Ismayil','Surname'=>'Nagiyev','Username'=>'ISOMARCO']);
         output( DB::Users()->result() );
         Masterpage::title(ML::select('SignIn'));
     }
