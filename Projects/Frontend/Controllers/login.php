68f440ceb0101d9fdd58f676bd6bbc7d6f010c31
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-12 13:56:02 +0400

    1.0

--- Projects/Frontend/Controllers/login.php
+++ Projects/Frontend/Controllers/login.php
@@ -4,7 +4,7 @@ class login extends Controller
 {
     public function main()
     {
-        output( DB::Users()->stringQuery() );
+        output( DB::Users()->result() );
         Masterpage::title(ML::select('SignIn'));
     }
 }
