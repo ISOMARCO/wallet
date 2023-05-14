f4cf8245f8e044efc88ee7dea07bd3ad774d6707
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-14 15:00:45 +0400

    1

--- Projects/Frontend/Models/Migrations/Users.php
+++ Projects/Frontend/Models/Migrations/Users.php
@@ -16,7 +16,7 @@ class InternalMigrateUsers extends ZN\Database\Migration
 			'Name' => [DB::varchar(20),DB::null()],
 			'Surname' => [DB::varchar(20),DB::null()],
 			'Password' => [DB::varchar(200),DB::null()],
-			'Ban' => [DB::varchar(10),DB::null(),DB::defaultValue(false)],
+			'Ban' => [DB::tinyInt(1),DB::null(),DB::defaultValue(false)],
 			'Role' => [DB::varchar(10),DB::defaultValue('USER')]
 		]);
 	}
