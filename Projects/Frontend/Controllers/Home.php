889a56c663ff0a2231e7c34c7766029884bb2eeb
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 01:41:22 +0400

    1

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends Controller
     public function main(string ...$parameters)
     {
         Masterpage::title('Home');
-        output( Cache::select('userInfo_'.Session::Uid()) );
+        output( Cache::select('userInfo_'.Session::Uid())->Name );
     } 
     public function exit()
     {
