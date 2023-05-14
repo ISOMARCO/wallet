e01305117dc52aa0e8376ec2a94b564ce0545639
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-14 15:03:48 +0400

    1

--- Projects/Frontend/Controllers/login.php
+++ Projects/Frontend/Controllers/login.php
@@ -1,11 +1,11 @@
 <?php namespace Project\Controllers;
-use ML,MigrateUsers;
+use ML,MigrateUsers,DB;
 class login extends Controller
 {
     public function main()
     {
         Masterpage::title(ML::select('SignIn'));
         echo MigrateUsers::up();
-        echo intval('0');
+        echo DB::defaultValue(0);
     }
 }
