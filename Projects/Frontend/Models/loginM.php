509fe257660a449807e71329f9f85ab79e688906
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 01:38:07 +0400

    1

--- Projects/Frontend/Models/loginM.php
+++ Projects/Frontend/Models/loginM.php
@@ -12,11 +12,12 @@ class loginM extends Model
         if($query->totalRows())
         {
             $row = $query->row();
+            $cacheData = ['Name' => $row->Name,'Surname' => $row->Surname];
             if($rememberMe)
             {
                 Cookie::insert( hash('sha256',md5('Email')),encrypt($email),(60*60*24*365) );
                 Cookie::insert( hash('sha256',md5('Password')),encrypt($password),(60*60*24*365) );
-                Cache::insert('userInfo_'.$row->Uid,$row,120);
+                Cache::insert('userInfo_'.$row->Uid,$cacheData,120);
             }
             return $row;
         }
