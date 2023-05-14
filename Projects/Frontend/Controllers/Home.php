01d9a77a44fa832eee5fe6b49c558dd393996b17
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-14 23:26:59 +0400

    1

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -6,6 +6,13 @@ class Home extends Controller
     {
         Masterpage::title('Welcome to The World of Simplicity');
     } 
+    public function exit()
+    {
+        Cookie::deleteAll();
+        Session::deleteAll();
+        redirect(URL::base('login'));
+        exit;
+    }
     public function s404()
     {
         Masterpage::title('404! File Not Found');
