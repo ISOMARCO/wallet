5fd0f9f524116d276ca6f1486af8052e496dacce
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-14 15:47:12 +0400

    1

--- Projects/Frontend/Views/login/main.wizard.php
+++ Projects/Frontend/Views/login/main.wizard.php
@@ -77,7 +77,7 @@
     $("#email,#password").on("keyup",function(){
       if($(this).val() == "")
       {
-        $(this).parent('.input-group').css({"border-color":"red"});
+        $(this).parent('.input-group').addClass("border-danger");
       }
     });
   });
