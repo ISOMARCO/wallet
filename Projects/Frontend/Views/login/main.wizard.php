9f949dbcddda1a55a07c29c0f1532a2618f65d70
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-14 15:52:36 +0400

    1

--- Projects/Frontend/Views/login/main.wizard.php
+++ Projects/Frontend/Views/login/main.wizard.php
@@ -78,6 +78,7 @@
       if($(this).val() == "")
       {
         $(this).attr("placeholder","Email boş buraxıla bilməz.");
+        $(this+"::placeholder").css({"color","red"});
       }
     });
   });
