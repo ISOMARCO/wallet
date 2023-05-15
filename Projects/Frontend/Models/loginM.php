1303ed7d7712e6ef07d485177f63d28a6c3ec9d9
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 10:15:55 +0400

    1

--- Projects/Frontend/Models/loginM.php
+++ Projects/Frontend/Models/loginM.php
@@ -5,22 +5,32 @@ class loginM extends Model
     public static function login($email,$password,$rememberMe=false)
     {
         $password = hash('sha256',md5($password));
-        $query = DB::select('Uid','Username','Name','Surname','Lang','Role')->
+        $query = DB::select('Username','Name','Surname','Lang','Role')->
         where('Email',$email)->
         where('Password',$password)->
         Users();
         if($query->totalRows())
         {
             $row = $query->row();
-            #$cacheData = json_encode(['Name' => $row->Name,'Surname' => $row->Surname]);
             if($rememberMe)
             {
                 Cookie::insert( hash('sha256',md5('Email')),encrypt($email),(60*60*24*365) );
                 Cookie::insert( hash('sha256',md5('Password')),encrypt($password),(60*60*24*365) );
             }
-            Cache::insert('userInfo_'.$row->Uid,json_encode($row),(60*60*24*365));
+            self::makeUserCache($row);
             return $row;
         }
         return NULL;
+    }
+    public static function makeUserCache($data = NULL)
+    {
+        if($data == NULL)
+        {
+            $row = DB::select('Username','Name','Surname','Lang','Role')->
+            where('Uid',Session::Uid())->
+            Users()->row();
+            return Cache::insert('userInfo_'.Session::Uid(),json_encode($row),(60*60*24*365));
+        }
+        return Cache::insert('userInfo_'.Session::Uid(),json_encode($data),(60*60*24*365));
     }
 }
