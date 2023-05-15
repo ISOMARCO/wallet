96f66f1916ddf7e9e326b0f73b205902a609b3dc
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 12:27:44 +0400

    1

--- Projects/Frontend/Views/accounts/main.wizard.php
+++ Projects/Frontend/Views/accounts/main.wizard.php
@@ -5,11 +5,11 @@
     <div class="container-fluid">
     <div class="row mb-2">
         <div class="col-sm-6">
-        <h1>Blank Page accounts</h1>
+        <h1>Accounts</h1>
         </div>
         <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{URL::base('home')}}">{{ML::select('Home')}}</a></li>
+            <li class="breadcrumb-item"><a href="{{URL::base('accounts')}}">{{ML::select('Accounts')}}</a></li>
             <li class="breadcrumb-item active">Blank Page</li>
         </ol>
         </div>
@@ -17,7 +17,7 @@
     </div><!-- /.container-fluid -->
 </section>
 <section class="content">
-Deneme yazi
+    Accounts
 
 </section>
 </div>
