ccba725a1802d10a3e5e648cdb47924d2bdbab4a
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 16:48:51 +0400

    1

--- Projects/Frontend/Views/accounts/main.wizard.php
+++ Projects/Frontend/Views/accounts/main.wizard.php
@@ -122,7 +122,7 @@
 </section>
 </div>
 <script>
-    //$(".select2").select2();
+$(".select2").select2();
 $(document).ready(function(){
     $("#searchButton").on("click",function(){
         if($("#searchBody").attr("style")) $("#searchBody").removeAttr("style");
