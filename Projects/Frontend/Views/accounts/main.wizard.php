10c5f550e2634fbc7e3da687e3d02b7b39e04b58
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 22:21:00 +0400

    1

--- Projects/Frontend/Views/accounts/main.wizard.php
+++ Projects/Frontend/Views/accounts/main.wizard.php
@@ -118,6 +118,31 @@
         Footer
         </div>
     </div>
+    
+    <div class="card collapsed-card">
+        <div class="card-header">
+        <h3 class="card-title">
+            <img src="{{FILES_DIR.'Accounts/bitcoin.svg'}}" style="width:48px;height:30px;" class="img-fluid" alt="Unibank Ucard">
+            Bitcoin
+        </h3>
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-plus"></i>
+            </button>
+            <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+        </div>
+        <div class="card-body">
+        Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+        Footer
+        </div>
+    </div>
 </div>
 </section>
 </div>
