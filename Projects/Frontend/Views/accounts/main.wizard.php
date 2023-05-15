b23398c06161d233b45c2b32db1cdc45f2f8c650
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 13:37:56 +0400

    1

--- Projects/Frontend/Views/accounts/main.wizard.php
+++ Projects/Frontend/Views/accounts/main.wizard.php
@@ -20,7 +20,7 @@
 <div class="card collapsed-card">
     <div class="card-header">
     <h3 class="card-title">
-        <img src="{{FILES_DIR.'Accounts/rabitabank.webp'}}" style="width:30px;height:30px;" class="img-fluid" alt="Rabita Bank">
+        <img src="{{FILES_DIR.'Accounts/rabitabank.svg'}}" style="width:30px;height:30px;" class="img-fluid" alt="Rabita Bank">
         Rabita Bank Credit
     </h3>
 
@@ -43,29 +43,5 @@
 </div>
 
 
-<div class="card collapsed-card">
-    <div class="card-header">
-    <h3 class="card-title">
-        <img src="{{FILES_DIR.'Accounts/KapitalBankBirbank.svg'}}" style="width:40px;height:25px;" class="img-fluid" alt="Kapital Bank Birbank">
-        Kapital Bank Birbank
-    </h3>
-
-    <div class="card-tools">
-        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-        <i class="fas fa-plus"></i>
-        </button>
-        <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
-        <i class="fas fa-times"></i>
-        </button>
-    </div>
-    </div>
-    <div class="card-body">
-    Start creating your amazing application!
-    </div>
-    <!-- /.card-body -->
-    <div class="card-footer">
-    Footer
-    </div>
-</div>
 </section>
 </div>
