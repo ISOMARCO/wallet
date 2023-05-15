343c543fa2c7518aba2375f66aec668cad35c38f
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 12:20:00 +0400

    1

--- Projects/Frontend/Views/Sections/footer.wizard.php
+++ Projects/Frontend/Views/Sections/footer.wizard.php
@@ -5,9 +5,9 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 0.1
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="javascript:void(0)">Wallet ISO.COM.AZ</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023 <a href="javascript:void(0)">Wallet ISO.COM.AZ</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
