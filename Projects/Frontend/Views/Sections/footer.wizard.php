c8c7930c6d33b641ddde7f2e94c449ca2402467c
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 17:22:21 +0400

    1

--- Projects/Frontend/Views/Sections/footer.wizard.php
+++ Projects/Frontend/Views/Sections/footer.wizard.php
@@ -17,7 +17,5 @@
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
-<!-- select2 -->
-<script src="plugins/select2/js/select2.full.min.js"></script>
 </body>
 </html>
--- Projects/Frontend/Views/accounts/main.wizard.php
+++ Projects/Frontend/Views/accounts/main.wizard.php
@@ -121,6 +121,8 @@
 </div>
 </section>
 </div>
+<!-- select2 -->
+<script src="plugins/select2/js/select2.min.js"></script>
 <script>
 $(".select2").select2();
 $(document).ready(function(){
