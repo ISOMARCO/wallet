366bad2c1545d8ef3d8330337e2dbe7723ca582a
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 16:05:56 +0400

    1

--- Projects/Frontend/Views/accounts/main.wizard.php
+++ Projects/Frontend/Views/accounts/main.wizard.php
@@ -17,53 +17,63 @@
     </div><!-- /.container-fluid -->
 </section>
 <section class="content">
-<div class="card collapsed-card">
-    <div class="card-header">
-    <h3 class="card-title">
-        <img src="{{FILES_DIR.'Accounts/rabitabank.svg'}}" style="width:48px;height:30px;" class="img-fluid" alt="Rabita Bank">
-        Rabita Bank Credit
-    </h3>
-
-    <div class="card-tools">
-        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-        <i class="fas fa-plus"></i>
-        </button>
-        <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
-        <i class="fas fa-times"></i>
-        </button>
-    </div>
-    </div>
-    <div class="card-body">
-    Start creating your amazing application!
-    </div>
-    <!-- /.card-body -->
-    <div class="card-footer">
-    Footer
-    </div>
+<div class="card">
+<div class="card-header">
+    <h5 class="float-left"><i class="fas fa-arrow-circle-right"></i> İcarəyə Verilən</h5>
+    <span class="float-right">
+        <a href="{{URL::base('icare/elave_et?nov=icarə')}}" class="btn btn-outline-primary">Yeni Qaimə <i class="fas fa-plus"></i></a> 
+        <button type="button" class="btn btn-outline-secondary" id="axtaris">Axtarış <i class="fas fa-search"></i></button>
+    </span>
 </div>
+<div class="card-body">
+    <div class="card collapsed-card">
+        <div class="card-header">
+        <h3 class="card-title">
+            <img src="{{FILES_DIR.'Accounts/rabitabank.svg'}}" style="width:48px;height:30px;" class="img-fluid" alt="Rabita Bank">
+            Rabita Bank Credit
+        </h3>
 
-<div class="card collapsed-card">
-    <div class="card-header">
-    <h3 class="card-title">
-        <img src="{{FILES_DIR.'Accounts/birbank.svg'}}" style="width:30px;height:48px;" class="img-fluid" alt="Kapital Bank Birbank">
-        Kapital Bank Birbank
-    </h3>
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-plus"></i>
+            </button>
+            <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+        </div>
+        <div class="card-body">
+        Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+        Footer
+        </div>
+    </div>
 
-    <div class="card-tools">
-        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-        <i class="fas fa-plus"></i>
-        </button>
-        <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
-        <i class="fas fa-times"></i>
-        </button>
-    </div>
-    </div>
-    <div class="card-body">
-    Start creating your amazing application!
-    </div>
-    <!-- /.card-body -->
-    <div class="card-footer">
-    Footer
+    <div class="card collapsed-card">
+        <div class="card-header">
+        <h3 class="card-title">
+            <img src="{{FILES_DIR.'Accounts/birbank.svg'}}" style="width:30px;height:48px;" class="img-fluid" alt="Kapital Bank Birbank">
+            Kapital Bank Birbank
+        </h3>
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-plus"></i>
+            </button>
+            <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+        </div>
+        <div class="card-body">
+        Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+        Footer
+        </div>
     </div>
 </div>
 </section>
