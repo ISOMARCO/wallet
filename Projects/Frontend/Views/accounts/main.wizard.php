cc21fa9ea9338c4f4b0a83c812cd039c948143ed
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-15 12:54:24 +0400

    1

--- Projects/Frontend/Views/accounts/main.wizard.php
+++ Projects/Frontend/Views/accounts/main.wizard.php
@@ -17,7 +17,26 @@
     </div><!-- /.container-fluid -->
 </section>
 <section class="content">
-    Accounts
+<div class="card collapsed-card">
+    <div class="card-header">
+    <h3 class="card-title">Rabita Bank Credit</h3>
 
+    <div class="card-tools">
+        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+        <i class="fas fa-plus"></i>
+        </button>
+        <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
+        <i class="fas fa-times"></i>
+        </button>
+    </div>
+    </div>
+    <div class="card-body">
+    Start creating your amazing application!
+    </div>
+    <!-- /.card-body -->
+    <div class="card-footer">
+    Footer
+    </div>
+</div>
 </section>
 </div>
