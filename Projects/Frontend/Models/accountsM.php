94a2b596fc715187e66f1521459ad95fab91225a
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-17 11:39:04 +0400

    1

--- Projects/Frontend/Models/accountsM.php
+++ Projects/Frontend/Models/accountsM.php
@@ -15,4 +15,15 @@ class accountsM extends Model
     {
         return DB::select('Code','Name','Picture','Style')->where('Active',1)->Banks()->result();
     }
+    public static function addAccount($data=[])
+    {
+        $user = Session::Uid();
+        if(!empty($data['User'])) $user = $data['User'];
+        return DB::insert('Account',[
+            'Uid' => uniqid(uniqid().'_'),
+            'Name' => $data['Name'],
+            'Bank_Code' => $data['Bank_Code'],
+            'User' => $user
+        ]);
+    }
 }
