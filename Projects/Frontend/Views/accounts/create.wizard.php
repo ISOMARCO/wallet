1dbfca7b4b0921b2aba1444a52bc3a141ff5cc00
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-17 16:07:55 +0400

    1

--- Projects/Frontend/Views/accounts/create.wizard.php
+++ Projects/Frontend/Views/accounts/create.wizard.php
@@ -16,65 +16,67 @@
     </div>
     </div><!-- /.container-fluid -->
 </section>
-<section class="content">
-    <div class="card card-info">
-        <div class="card-header">Account Əlavə Et</div>
-        <div class="card-body">
-            <span id="msg" style="font-weight:bold"></span>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <label for="name" class="input-group-text">Bank&nbsp;&nbsp;<img src="" style="width:20px;height:20px;" id="bankNameImg"></label>
+<section class="content"> 
+    <form method="post">
+        <div class="card card-info">
+            <div class="card-header">Account Əlavə Et</div>
+            <div class="card-body">
+                <span id="msg" style="font-weight:bold"></span>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label for="name" class="input-group-text">Bank&nbsp;&nbsp;<img src="" style="width:20px;height:20px;" id="bankNameImg"></label>
+                    </div>
+                    <select name="name" id="name" class="form-control">
+                        @foreach($banks as $value)
+                            <option value="{{$value->Code}}" data-image="{{$value->Picture}}">{{$value->Name}}</option>
+                        @endforeach
+                    </select>
                 </div>
-                <select name="name" id="name" class="form-control">
-                    @foreach($banks as $value)
-                        <option value="{{$value->Code}}" data-image="{{$value->Picture}}">{{$value->Name}}</option>
-                    @endforeach
-                </select>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label for="type" class="input-group-text">Type &nbsp;<i class="far fa-credit-card"></i></label>
+                    </div>
+                    <select name="type" id="type" class="form-control">
+                        <option value="DEBIT">Debit</option>
+                        <option value="CREDIT">Credit</option>
+                    </select>
+                </div>
+                <div class="input-group mb-3" id="credit_amount_div" style="display:none">
+                    <div class="input-group-prepend">
+                        <label for="credit_amount" class="input-group-text">Credit Amount<i class="fas fa-hand-holding-usd"></i></label>
+                    </div>
+                    <input type="number" class="form-control" id="credit_amount" name="credit_amount" value="500">
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label for="balance" class="input-group-text">Balance<i class="fas fa-hand-holding-usd"></i></label>
+                    </div>
+                    <input type="number" class="form-control" id="balance" name="balance" value="0">
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label for="currency" class="input-group-text">Currency &nbsp;<i class="fas fa-dollar-sign"></i></label>
+                    </div>
+                    <select name="currency" id="currency" class="form-control">
+                        <option value="AZN">AZN</option>
+                        <option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                        <option value="TL">TL</option>
+                    </select>
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label for="qeyd" class="input-group-text">Qeyd <i class="fas fa-clipboard"></i></label>
+                    </div>
+                    <textarea name="qeyd" id="qeyd" rows="2" class="form-control"></textarea>
+                </div>
             </div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <label for="type" class="input-group-text">Type &nbsp;<i class="far fa-credit-card"></i></label>
-                </div>
-                <select name="type" id="type" class="form-control">
-                    <option value="DEBIT">Debit</option>
-                    <option value="CREDIT">Credit</option>
-                </select>
-            </div>
-            <div class="input-group mb-3" id="credit_amount_div" style="display:none">
-                <div class="input-group-prepend">
-                    <label for="credit_amount" class="input-group-text">Credit Amount<i class="fas fa-hand-holding-usd"></i></label>
-                </div>
-                <input type="number" class="form-control" id="credit_amount" name="credit_amount" value="500">
-            </div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <label for="balance" class="input-group-text">Balance<i class="fas fa-hand-holding-usd"></i></label>
-                </div>
-                <input type="number" class="form-control" id="balance" name="balance" value="0">
-            </div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <label for="currency" class="input-group-text">Currency &nbsp;<i class="fas fa-dollar-sign"></i></label>
-                </div>
-                <select name="currency" id="currency" class="form-control">
-                    <option value="AZN">AZN</option>
-                    <option value="USD">USD</option>
-                    <option value="EUR">EUR</option>
-                    <option value="TL">TL</option>
-                </select>
-            </div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <label for="qeyd" class="input-group-text">Qeyd <i class="fas fa-clipboard"></i></label>
-                </div>
-                <textarea name="qeyd" id="qeyd" rows="2" class="form-control"></textarea>
+            <div class="card-footer">
+                <button type="button" class="btn btn-info" id="insert">GÖNDƏR <i class="far fa-paper-plane"></i></button>
+                <button type="button" class="btn btn-default float-right" id="reset">SIFIRLA <i class="fas fa-ban"></i></button>
             </div>
         </div>
-        <div class="card-footer">
-            <button type="button" class="btn btn-info" id="gonder">GÖNDƏR <i class="far fa-paper-plane"></i></button>
-            <button type="button" class="btn btn-default float-right" id="reset">SIFIRLA <i class="fas fa-ban"></i></button>
-        </div>
-    </div>
+    </form>
 </section>
 </div>
 <script>
@@ -101,6 +103,9 @@ $(document).ready(function(){
     });
     $("#reset").on("click",function(){
         $("input, textarea").val("");
+    });
+    $("#insert").on("click",function(){
+
     });
 }); 
 </script>
