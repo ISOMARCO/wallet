0eef9ee8cba4a76bdc6d8bfe689a70849200e44e
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-17 12:52:54 +0400

    1

--- Projects/Frontend/Models/Migrations/Account.php
+++ Projects/Frontend/Models/Migrations/Account.php
@@ -16,6 +16,7 @@ class InternalMigrateAccount extends ZN\Database\Migration
 			'Bank_Code' => [DB::varchar(20),DB::null()],
 			'Type' => [DB::varchar(50),DB::null(),DB::defaultValue('Debit')],
 			'Credit_Amount' => [DB::double(),DB::null(),DB::defaultValue('500')],
+			'Currency' => [DB::varchar(5),DB::null(),DB::defaultValue('AZN')],
 			'Balance' => [DB::double(),DB::null(),DB::defaultValue(' 0')],
 			'About' => [DB::varchar(250),DB::null()],
 			'User' => [DB::varchar(30),DB::null()],
