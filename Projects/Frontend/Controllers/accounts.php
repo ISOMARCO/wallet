2971b89464d6dd231b39fbe1266c45587ac504cc
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-17 10:23:31 +0400

    1

--- Projects/Frontend/Controllers/accounts.php
+++ Projects/Frontend/Controllers/accounts.php
@@ -5,7 +5,7 @@ class accounts extends Controller
     public function main()
     {
         Masterpage::title('Accounts');
-        #accountsM::addBank(['Code' => 'RABITABANK','Name' => 'Rabitə Bank', 'Picture' => 'rabitabank.svg', 'Style' => 'width:30px;height:30px;']);
+        accountsM::addBank(['Code' => 'KAPITALBANK','Name' => 'Kapital Bank', 'Picture' => 'birbank.png', 'Style' => 'width:30px;height:30px;']);
     }
     public function create()
     {
