6befe66da68fa2b5f0f93edd6e7f2d7044dd544c
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-17 11:24:44 +0400

    1

--- Projects/Frontend/Controllers/accounts.php
+++ Projects/Frontend/Controllers/accounts.php
@@ -1,10 +1,11 @@
 <?php namespace Project\Controllers;
-use Session,Cache,accountsM;
+use Session,Cache,accountsM,MigrateAccount;
 class accounts extends Controller
 {
     public function main()
     {
         Masterpage::title('Accounts');
+        MigrateAccount::up();
         #accountsM::addBank(['Code' => 'KAPITALBANK','Name' => 'Kapital Bank', 'Picture' => 'birbank.png', 'Style' => 'width:30px;height:30px;']);
     }
     public function create()
