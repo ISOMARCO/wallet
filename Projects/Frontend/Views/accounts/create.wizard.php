3cc01702a1d55394b71585219cb87e09cf5aac69
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-17 10:13:56 +0400

    1

--- Projects/Frontend/Views/accounts/create.wizard.php
+++ Projects/Frontend/Views/accounts/create.wizard.php
@@ -25,7 +25,7 @@
                 <div class="input-group-prepend">
                     <label for="name" class="input-group-text">Name <i class="fas fa-piggy-bank"></i></label>
                 </div>
-                <select name="name" id="select2" class="form-control">
+                <select name="name" id="name" class="form-control">
                     @foreach($banks as $value)
                         <option value="{{$value->Code}}"><img src="{{$value->Picture}}" style="{{$value->Style}}"> {{$value->Name}}</option>
                     @endforeach
