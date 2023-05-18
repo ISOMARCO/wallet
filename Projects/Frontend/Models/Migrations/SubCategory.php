587cb3c30ae7db0d01b09bb3794a29bc5f0b679c
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-18 15:15:12 +0400

    1

---
+++ Projects/Frontend/Models/Migrations/SubCategory.php
@@ -0,0 +1,29 @@
+<?php
+class InternalMigrateSubCategory extends ZN\Database\Migration
+{
+	# Class/Table Name
+	const table = __CLASS__;
+
+	# Up
+	public function up()
+	{
+		# Default Query
+		return $this->createTable
+		([
+			'Id' => [DB::int(11),DB::primaryKey(), DB::autoIncrement(),DB::notNull()],
+            'Uid' => [DB::varchar(30),DB::unique(),DB::notNull()]#,
+            #'Category_Uid' => [DB::varchar(30),DB::null()],
+			// 'Parent_Uid' => [DB::varchar(30),DB::null()],
+            // 'Child_Uid' => [DB::varchar(30),DB::null()],
+            // 'User' => [DB::varchar(30),DB::null()],
+            // 'Active' => [DB::tinyInt(1),DB::defaultValue(' 0')]
+		]);
+	}
+
+	# Down
+	public function down()
+	{
+		# Default Query
+		return $this->dropTable();
+	}
+}
