a160c8eb5706d3b8299dec60ad49ef7ef8e0c786
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-19 10:24:50 +0400

    1

--- Projects/Frontend/Views/Category/main.wizard.php
+++ Projects/Frontend/Views/Category/main.wizard.php
@@ -24,7 +24,7 @@
         </thead>
         <tbody>
             <tr>
-                <td>Market</td>
+                <td><i class="fas fa-arrow-right"></i> Market</td>
                 <td><button type="button" class="btn btn-primary">Edit</button></td>
             </tr>
         </tbody>
