c0fc4806e5644556ddf99e11c27712646582acec
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-19 10:19:20 +0400

    1

--- Projects/Frontend/Views/Category/main.wizard.php
+++ Projects/Frontend/Views/Category/main.wizard.php
@@ -15,115 +15,22 @@
     </div><!-- /.container-fluid -->
 </section>
 <section class="content">
-
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">{{ML::select('Category')}}</h3>
-            </div>
-            <div class="card-body p-0">
-            <table class="table table-hover">
-                <tbody>
-                <tr>
-                    <td class="border-0">183</td>
-                </tr>
-                <tr data-widget="expandable-table" aria-expanded="false">
-                    <td>
-                    <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
-                    219
-                    </td>
-                </tr>
-                <tr class="expandable-body">
-                    <td>
-                    <div class="p-0">
-                        <table class="table table-hover">
-                        <tbody>
-                            <tr data-widget="expandable-table" aria-expanded="false">
-                            <td>
-                                <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
-                                219-1
-                            </td>
-                            </tr>
-                            <tr class="expandable-body">
-                            <td>
-                                <div class="p-0">
-                                <table class="table table-hover">
-                                    <tbody>
-                                    <tr>
-                                        <td>219-1-1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>219-1-2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>219-1-3</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                </div>
-                            </td>
-                            </tr>
-                            <tr data-widget="expandable-table" aria-expanded="false">
-                            <td>
-                                <button type="button" class="btn btn-primary p-0">
-                                <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
-                                </button>
-                                219-2
-                            </td>
-                            </tr>
-                            <tr class="expandable-body">
-                            <td>
-                                <div class="p-0">
-                                <table class="table table-hover">
-                                    <tbody>
-                                    <tr>
-                                        <td>219-2-1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>219-2-2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>219-2-3</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                </div>
-                            </td>
-                            </tr>
-                            <tr>
-                            <td>219-3</td>
-                            </tr>
-                        </tbody>
-                        </table>
-                    </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>657</td>
-                </tr>
-                <tr>
-                    <td>175</td>
-                </tr>
-                <tr>
-                    <td>134</td>
-                </tr>
-                <tr>
-                    <td>494</td>
-                </tr>
-                <tr>
-                    <td>832</td>
-                </tr>
-                <tr>
-                    <td>982</td>
-                </tr>
-                </tbody>
-            </table>
-            </div>
-            <!-- /.card-body -->
-        </div>
-    </div>
-</div>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>    
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>Market</th>
+                <th><button type="btn btn-primary">Edit</button></th>
+            </tr>
+        </tbody>
+    </table>
 
 </section>
 </div>
