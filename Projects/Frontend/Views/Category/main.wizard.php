4305f43592c8eb5ad906a65cd9f72f6ffd51d144
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-20 16:51:54 +0400

    1

--- Projects/Frontend/Views/Category/main.wizard.php
+++ Projects/Frontend/Views/Category/main.wizard.php
@@ -39,7 +39,7 @@
 <script>
 $(document).ready(function(){
     $("#categoryName td i").on("click",function(){
-        alert($(this).attr('class'));
+        alert($(this).attr('id'));
     });
 });
 </script>
