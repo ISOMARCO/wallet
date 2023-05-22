0f8e5e725798b4a4362a19ec728904fc7b6ccffb
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-22 09:53:40 +0400

    1

--- Projects/Frontend/Views/Category/main.wizard.php
+++ Projects/Frontend/Views/Category/main.wizard.php
@@ -1,9 +1,11 @@
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
+<!-- Content Header (Page header) -->
 <section class="content-header">
     <div class="container-fluid">
     <div class="row mb-2">
         <div class="col-sm-6">
-        <!-- <h1>Accounts</h1> -->
+        <h1>Accounts</h1>
         </div>
         <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
@@ -15,31 +17,65 @@
     </div><!-- /.container-fluid -->
 </section>
 <section class="content">
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th class="col-md-11">Name</th>
-                <th class="col-md-1">Action</th>
-            </tr>    
-        </thead>
-        <tbody>
-            <tr id="categoryName" class="c1">
-                <td><i class="fas fa-arrow-right" style="cursor:pointer"></i>&nbsp;Market</td>
-                <td><button type="button" class="btn btn-link">Edit</button></td>
-            </tr>
-            <tr id="categoryName" class="c2">
-                <td><i class="fas fa-arrow-right" style="cursor:pointer"></i>&nbsp;Market</td>
-                <td><button type="button" class="btn btn-link">Edit</button></td>
-            </tr>
-        </tbody>
-    </table>
+<div class="card">
+<div class="card-header">
+    <span class="float-right">
+        <a href="{{URL::base('accounts/create')}}" class="btn btn-outline-primary">Create Account <i class="fas fa-plus"></i></a> 
+        <button type="button" class="btn btn-outline-secondary" id="searchButton">Search <i class="fas fa-search"></i></button>
+    </span>
+</div>
+<div class="card-body">
+    <div class="row mb-3" id="searchBody" style="display:none">
+        <div class="col-md-2"><input type="text" name="qaime" class="form-control" placeholder="Qaimə №" autocomplete="off"></div>
+        <div class="col-md-3"><input type="date" name="tarix" id="tarix" class="form-control"></div>
+        <div class="col-md-3">
+            <select class="select2" name="musteri" style="width:100%">
+                <option value="">Heç biri</option>
+                <option value="Ismayil">Ismayil</option>
+                
+            </select>
+        </div>
+        <div class="col-md-3">
+            <select class="select2" id="select2" name="erazi" style="width:100%">
+                <option value="">Heç biri</option>
+                <option value="Baku">Baku</option>
+            </select>
+        </div>
+        <div class="col-md-1"><button type="submit" class="btn btn-outline-secondary"><i class="fas fa-search"></i></button></div>
+    </div>
+    <div class="card collapsed-card">
+        <div class="card-header">
+        <h3 class="card-title">
+            <i class="fas fa-shopping-basket"></i>
+            Market
+        </h3>
 
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-plus"></i>
+            </button>
+            <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+        </div>
+        <div class="card-body">
+            Xerclenen: 100
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-primary" style="float:left">Edit&nbsp;<i class="fas fa-edit"></i></button>
+            <button type="button" class="btn btn-danger" style="float:right" onclick="return confirm('Are you sure?')">Delete&nbsp;<i class="fas fa-trash"></i></button>
+        </div>
+    </div>
+</div>
 </section>
 </div>
 <script>
+$(".select2").select2();
 $(document).ready(function(){
-    $("#categoryName td i").on("click",function(){
-        var id = $(this).attr('id');
+    $("#searchButton").on("click",function(){
+        if($("#searchBody").attr("style")) $("#searchBody").removeAttr("style");
+        else $("#searchBody").attr("style","display:none");
     });
-});
+}); 
 </script>
