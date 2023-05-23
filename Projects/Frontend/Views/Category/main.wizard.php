e6b9c292050731e4242e7e9c8bbb5e2002dc7e42
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-23 16:17:32 +0400

    1

--- Projects/Frontend/Views/Category/main.wizard.php
+++ Projects/Frontend/Views/Category/main.wizard.php
@@ -163,7 +163,7 @@
     </div>
 
 
-    <div class="card collapsed-card ml-2">
+    <div class="card collapsed-card" style="margin-left:2px;">
         <div class="card-header">
         <h3 class="card-title">
         <i class="fab fa-amazon"></i>
@@ -285,7 +285,7 @@
         </div>
     </div>
     
-    <div class="card collapsed-card ml-4">
+    <div class="card collapsed-card" style="margin-left:4px;">
         <div class="card-header">
         <h3 class="card-title">
         <i class="fab fa-amazon"></i>
