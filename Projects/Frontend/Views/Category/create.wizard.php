055b6103ffff7869024fe34952f744a4c4f0e9e2
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-24 14:02:52 +0400

    1

--- Projects/Frontend/Views/Category/create.wizard.php
+++ Projects/Frontend/Views/Category/create.wizard.php
@@ -35,47 +35,9 @@
                 </div>
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
-                        <label for="type" class="input-group-text">Type &nbsp;<i class="far fa-credit-card"></i></label>
-                    </div>
-                    <select name="type" id="type" class="form-control">
-                        <option value="DEBIT">Debit</option>
-                        <option value="CREDIT">Credit</option>
-                    </select>
-                </div>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <label for="name" class="input-group-text">Name&nbsp;<i class="fas fa-piggy-bank"></i></label>
+                        <label for="type" class="input-group-text">Name&nbsp;<i class="fas fa-edit"></i></label>
                     </div>
                     <input type="text" class="form-control" id="name" name="name">
-                </div>
-                <div class="input-group mb-3" id="credit_amount_div" style="display:none">
-                    <div class="input-group-prepend">
-                        <label for="credit_amount" class="input-group-text">Credit Amount&nbsp;<i class="fas fa-hand-holding-usd"></i></label>
-                    </div>
-                    <input type="number" class="form-control" id="credit_amount" name="credit_amount" value="500">
-                </div>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <label for="balance" class="input-group-text">Balance&nbsp;<i class="fas fa-hand-holding-usd"></i></label>
-                    </div>
-                    <input type="number" class="form-control" id="balance" name="balance" value="0">
-                </div>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <label for="currency" class="input-group-text">Currency &nbsp;<i class="fas fa-dollar-sign"></i></label>
-                    </div>
-                    <select name="currency" id="currency" class="form-control">
-                        <option value="AZN">AZN</option>
-                        <option value="USD">USD</option>
-                        <option value="EUR">EUR</option>
-                        <option value="TL">TL</option>
-                    </select>
-                </div>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <label for="qeyd" class="input-group-text">Qeyd&nbsp;<i class="fas fa-clipboard"></i></label>
-                    </div>
-                    <textarea name="qeyd" id="qeyd" rows="2" class="form-control"></textarea>
                 </div>
             </div>
             <div class="card-footer">
