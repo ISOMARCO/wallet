e8716e9201b802f61f2a1f859be99e8db355fa97
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-24 13:43:53 +0400

    1

---
+++ Projects/Frontend/Models/categoryM.php
@@ -0,0 +1,8 @@
+<?php 
+
+public static function getCategory($type=NULL)
+{
+    $query = DB::where('User',Session::Uid());
+    if($type == NULL) $query->where('Type',$type);
+    return $query->Category()->stringQuery();
+}
