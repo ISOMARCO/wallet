30245e10d7886c39a6345e6b6b0f302bb7eb502c
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-24 14:28:39 +0400

    1

--- Projects/Frontend/Controllers/Category.php
+++ Projects/Frontend/Controllers/Category.php
@@ -16,6 +16,6 @@ class Category extends Controller
         Http::isAjax() or exit("Bad Request");
         $category = Post::category();
         $name = Post::name();
-        
+        echo json_encode(['error' => $category]);
     }
 }
