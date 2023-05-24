65f08df4c2dfe962152a2bc99a852142548004ab
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-24 17:13:39 +0400

    1

--- Projects/Frontend/Models/categoryM.php
+++ Projects/Frontend/Models/categoryM.php
@@ -10,7 +10,7 @@ class categoryM extends Model
     }
     public static function addCategory($data=[],$parentCategory=NULL)
     {
-        $transaction = DB::transaction(function() use($data,$parentCategory){
+        #$transaction = DB::transaction(function() use($data,$parentCategory){
             $uid = uniqid(uniqid().'_');
             $type = 'MAIN';
             if($parentCategory != NULL) $type = 'SUB'; 
@@ -30,7 +30,7 @@ class categoryM extends Model
                     'User' => Session::Uid()
                 ]);
             }
-        });
-        return $transaction;
+       # });
+       return true;
     }
 }
