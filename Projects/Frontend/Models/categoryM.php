67c5a2661e51521c26ed0560ff9b573f6648c89c
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-24 13:49:24 +0400

    1

--- Projects/Frontend/Models/categoryM.php
+++ Projects/Frontend/Models/categoryM.php
@@ -5,7 +5,7 @@ class categoryM extends Model
     public static function getCategory($type=NULL)
     {
         $query = DB::where('User',Session::Uid());
-        if($type == NULL) $query->where('Type',$type);
+        if($type != NULL) $query->where('Type',$type);
         return $query->Category()->stringQuery();
     }
 }
