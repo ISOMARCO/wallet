59281599ee7801f6371c20755b26081f50342439
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-24 17:51:46 +0400

    1

--- Projects/Frontend/Models/categoryM.php
+++ Projects/Frontend/Models/categoryM.php
@@ -13,7 +13,7 @@ class categoryM extends Model
         return DB::transaction(function() use($data,$parentCategory){
             $uid = uniqid(uniqid().'_');
             $type = 'MAIN';
-            if($parentCategory != NULL) $type = 'SUB'; 
+            #if($parentCategory != NULL) $type = 'SUB'; 
             DB::insert('Category',[
                 'Uid' => $uid,
                 'Name' => $data['Name'],
