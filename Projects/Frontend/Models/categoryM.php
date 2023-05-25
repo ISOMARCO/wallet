4e4fafb2e48b596df9c27b807c3b9d65d8804982
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-25 10:17:49 +0400

    1

--- Projects/Frontend/Models/categoryM.php
+++ Projects/Frontend/Models/categoryM.php
@@ -29,8 +29,7 @@ class categoryM extends Model
             #}
            # else 
             #{
-                return $parentCategory;
-                return DB::query( DB::select('Category_Uid')->where('Parent_Uid',$parentCategory)->Sub_Category()->stringQuery() )->resultJson();
+                return DB::select('Category_Uid')->where('Child_Uid',$parentCategory)->Sub_Category()->stringQuery()->resultJson();
             #}
             $transaction->insert('Sub_Category',[
                 'Uid' => uniqid(uniqid().'_'),
