fe8fb98f2e0c9fd8c028e88c6c0a8c51dddb6954
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-26 15:37:23 +0400

    1

--- Projects/Frontend/Views/Category/main.wizard.php
+++ Projects/Frontend/Views/Category/main.wizard.php
@@ -55,14 +55,14 @@
                         Market
                     </h3>
                 <!-- </div>   -->
-                <!-- <div class="col-md-1 col-sm-1 card-tools"> -->
+                <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                     <i class="fas fa-plus"></i>
                     </button>
                     <button type="button" class="btn btn-tool text-danger" data-card-widget="remove" title="Remove">
                     <i class="fas fa-times"></i>
                     </button>
-                <!-- </div> -->
+                </div>
             <!-- </div> -->
         </div>
         <div class="card-body">
