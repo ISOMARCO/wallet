49c7ce9a06911f4b5b977937ed15a78fd9ae2b9c
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-30 17:16:46 +0400

    1

--- Projects/Frontend/Controllers/operations.php
+++ Projects/Frontend/Controllers/operations.php
@@ -5,6 +5,7 @@ class operations extends Controller
     public function main()
     {
         Masterpage::title(ML::select('Operations'));
+        MigrateOperations::down();
         MigrateOperations::up();
     }
     public function create()
