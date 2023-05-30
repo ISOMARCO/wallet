501970a47e05c1ed42b3420415cbdf71acfc30df
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-05-30 17:16:24 +0400

    1

--- Projects/Frontend/Models/Migrations/Operations.php
+++ Projects/Frontend/Models/Migrations/Operations.php
@@ -8,15 +8,19 @@ class InternalMigrateOperations extends ZN\Database\Migration
 	public function up()
 	{
 		# Default Query
-		return $this->createTable
+
+		$this->createTable
 		([
 			'Id' => [DB::int(11),DB::primaryKey(), DB::autoIncrement(),DB::notNull()],
-            'Document_Number' => [DB::bigInt(20),DB::unique(),DB::notNull()],
+            'Document_Number' => [DB::bigInt(20),DB::unique(),DB::null()],
             'Type' => [DB::varchar(15), DB::null(), DB::defaultValue('Positive')],
+            'Description' => [DB::longText(),DB::null()],
             'Category_Uid' => [DB::varchar(30),DB::null()],
 			'Amount' => [DB::double(),DB::null(), DB::defaultValue(' 0')],
+            'Note' => [DB::text(1000), DB::null()],
             'User' => [DB::varchar(30), DB::null()]
 		]);
+        return DBForge::createFulltextIndex('DescriptionIndex','Operations','Description');
 	}
 
 	# Down
