481939b9d2220f2a37c21e3e4c8b6c6aa14677a0
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-02 11:20:11 +0400

    1

--- Projects/Frontend/Views/Category/create.wizard.php
+++ Projects/Frontend/Views/Category/create.wizard.php
@@ -55,7 +55,7 @@
                 </div>
                 <div class="row" id="category_icons">
                     @foreach($categoryImages as $value)
-                        <div class="col-1 col-sm-1 mb-2" id="icon"><img src="{{URL::base(FILES_DIR.'Categories/'.$value)}}" alt="" style="width:50px;height:50px;cursor: pointer;" title="{{str_replace('_',' ',ucfirst(explode('.',$value)[0]))}}"></div>
+                        <div class="col-1 col-sm-2 mb-2" id="icon"><img src="{{URL::base(FILES_DIR.'Categories/'.$value)}}" alt="" style="width:50px;height:50px;cursor: pointer;" title="{{str_replace('_',' ',ucfirst(explode('.',$value)[0]))}}"></div>
                     @endforeach
                 </div>
             </div>
