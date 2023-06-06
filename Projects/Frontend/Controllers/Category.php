3c3010ccfc62d665f0a59bb35a77393858bd2d72
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-07 00:26:46 +0400

    1

--- Projects/Frontend/Controllers/Category.php
+++ Projects/Frontend/Controllers/Category.php
@@ -1,12 +1,11 @@
 <?php namespace Project\Controllers;
-use ML,categoryM,Http,Post,Folder,MigrateCategory;
+use ML,categoryM,Http,Post,Folder;
 class Category extends Controller
 {
     public function main()
     {
         Masterpage::title(ML::select('Category'));
         View::categories(categoryM::getAllCategoryByUser());
-        #MigrateCategory::up();
     }
     public function create()
     {
