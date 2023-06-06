67c9f7e7ff1aaf31d2c529bccde971018bebd839
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-07 00:31:44 +0400

    1

--- Projects/Frontend/Models/Migrations/Category.php
+++ Projects/Frontend/Models/Migrations/Category.php
@@ -8,7 +8,7 @@ class InternalMigrateCategory extends ZN\Database\Migration
 	public function up()
 	{
 		# Default Query
-		$this->createTable
+		return $this->createTable
 		([
 			'Id' => [DB::int(11),DB::primaryKey(), DB::autoIncrement(),DB::notNull()],
             'Uid' => [DB::varchar(30),DB::unique(),DB::notNull()],
@@ -18,7 +18,7 @@ class InternalMigrateCategory extends ZN\Database\Migration
             'User' => [DB::varchar(30),DB::null()],
 			'Active' => [DB::tinyInt(1),DB::defaultValue('1')]
 		]);
-        return DBForge::createFulltextIndex('NameIndex','Category','Name');
+        //return DBForge::createFulltextIndex('NameIndex','Category','Name');
 	}
 
 	# Down
