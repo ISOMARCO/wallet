1f0ee93fad49c655045442eae3c32d02f53bf1bc
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-06 23:33:15 +0400

    1

--- Projects/Frontend/Views/Category/create.wizard.php
+++ Projects/Frontend/Views/Category/create.wizard.php
@@ -109,7 +109,7 @@ $(document).ready(function(){
     });
     $("#category").on("change",function(){
         $( "#category option:selected" ).each(function(){
-            //$("#name").val($("#bank option:selected").text()+" "+$("#type option:selected").text());
+            alert($(this).attr('value'));
             var img = $(this).attr("data-image");
             $(".fa-cube").hide();
             $("#category_image").attr("src","{{URL::base('"+img+"');}}");
