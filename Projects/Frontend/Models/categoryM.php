1ff9e9acf7659423c60afafa46e3ca23e85d6380
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-07 17:21:24 +0400

    1

--- Projects/Frontend/Models/categoryM.php
+++ Projects/Frontend/Models/categoryM.php
@@ -11,35 +11,14 @@ class categoryM extends Model
     public static function addCategory($data=[],$parentCategory=NULL)
     {
         $uid = uniqid(uniqid().'_');
-        $type = 'MAIN';
-        if($parentCategory != NULL) $type = 'SUB';
         $transaction = DB::transStart();
         $transaction->insert('Category',[
             'Uid' => $uid,
             'Name' => $data['Name'],
-            'Type' => $type,
-            'Entry_Type' => $data['Entry_Type'],
+            'Type' => $data['Type'],
             'Picture' => $data['Picture'],
             'User' => Session::Uid()
         ]);
-        if($parentCategory != NULL)
-        {
-            if($categoryType == 'MAIN')
-            {
-                $mainCategory = $parentCategory;
-            }
-            else 
-            {
-                $mainCategory = DB::query( DB::select('Category_Uid')->where('Child_Uid',$parentCategory)->Sub_Category()->stringQuery() )->value();
-            }
-            $transaction->insert('Sub_Category',[
-                'Uid' => uniqid(uniqid().'_'),
-                'Category_Uid' => $mainCategory,
-                'Parent_Uid' => $parentCategory,
-                'Child_Uid' => $uid,
-                'User' => Session::Uid()
-            ]);
-        }
         return $transaction->transEnd();
 
         
