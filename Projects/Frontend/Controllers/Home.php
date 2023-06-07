b50b4f24ae8ecf4dc8a18d289e8483f1ed30869d
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-07 17:17:46 +0400

    1

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -1,11 +1,10 @@
 <?php namespace Project\Controllers;
-use Cookie,Session,URL,Cache,MigrateCategory;
+use Cookie,Session,URL,Cache;
 class Home extends Controller
 {
     public function main(string ...$parameters)
     {
         Masterpage::title('Home');
-        MigrateCategory::up();
     } 
     public function exit()
     {
