e32f03178d51ccb445d91b2fd3cf627bebda86ad
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-09 11:53:10 +0400

    1

--- Projects/Frontend/Controllers/translation.php
+++ Projects/Frontend/Controllers/translation.php
@@ -7,13 +7,14 @@ class translation extends Controller
         Masterpage::title("Translation");
         $languages = translationM::languages()->result();
         $words = ML::selectAll();
-        /*foreach($words[$languages[0]->Code] as $key => $value)
+        foreach($words[$languages[0]->Code] as $key => $value)
         {
             foreach($languages as $lang)
             {
-                echo $words[$lang->Code][$key]."<br>";
+                echo $lang->Name." ".$words[$lang->Code][$key]."<br>";
             }
-        }*/
+        }
+        exit;
         View::words($words);
         View::languages($languages);
     }
