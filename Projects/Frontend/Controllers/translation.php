49cbdde3793a52f9ce98a862fe215fe072abfb55
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-09 11:06:24 +0400

    1

--- Projects/Frontend/Controllers/translation.php
+++ Projects/Frontend/Controllers/translation.php
@@ -5,9 +5,6 @@ class translation extends Controller
     public function main()
     {
         Masterpage::title("Translation");
-        $words = ML::selectAll();
-        $languages = translationM::languages()->result();
-        exit;
         View::words( ML::selectAll() );
         View::languages(translationM::languages()->result());
     }
