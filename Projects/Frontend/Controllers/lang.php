70c02b3c4966056e1eff4c529f15bc442120cb2c
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-09 10:31:41 +0400

    1

--- Projects/Frontend/Controllers/lang.php
+++ Projects/Frontend/Controllers/lang.php
@@ -5,7 +5,10 @@ class lang extends Controller
     public function main()
     {
         $all = ML::selectAll();
-        output($all);
+        for($i = 0; $i < count($all['en']); $i++)
+        {
+            echo $all['en'][$i]."<br>";1
+        }
         exit;
     }
 }
