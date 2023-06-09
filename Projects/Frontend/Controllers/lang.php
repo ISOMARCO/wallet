85b7b5ba77e678c7ac5df18ba95aca4edc9a8aec
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-09 10:29:58 +0400

    1

--- Projects/Frontend/Controllers/lang.php
+++ Projects/Frontend/Controllers/lang.php
@@ -4,13 +4,7 @@ class lang extends Controller
 {
     public function main()
     {
-        #ML::delete('en','Category');
-        #ML::delete('az','Category');
-        ML::insert('en','AddCategoryUnknownErrorMessage','Please try again');
-        ML::insert('az','AddCategoryUnknownErrorMessage','Zəhmət olmasa yenidən cəhd edin');
-        ML::insert('en','Error','Error');
-        ML::insert('az','Error','Xəta');
-        output(ML::selectAll());
+        echo ML::table();
         exit;
     }
 }
