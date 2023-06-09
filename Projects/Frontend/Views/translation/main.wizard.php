45e706b44180c304bbd15ae86455258be3471d40
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-09 12:02:52 +0400

    1

--- Projects/Frontend/Views/translation/main.wizard.php
+++ Projects/Frontend/Views/translation/main.wizard.php
@@ -93,7 +93,7 @@ $(document).ready(function(){
                 @endforeach
             </div>
             <div class="card-footer">
-                <button type="button" class="btn btn-primary" style="float:left">Edit&nbsp;<i class="fas fa-edit"></i></button>
+                <button type="button" class="btn btn-primary" style="float:left">Save&nbsp;<i class="fas fa-edit"></i></button>
                 <button type="button" class="btn btn-danger" style="float:right" onclick="return confirm('Are you sure?')">Delete&nbsp;<i class="fas fa-trash"></i></button>
             </div>
         </div>
