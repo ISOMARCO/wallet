00eab0b171c2a1e20194c1f9d42e56462affccdb
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-11 21:17:15 +0400

    1

--- Projects/Frontend/Controllers/Initialize.php
+++ Projects/Frontend/Controllers/Initialize.php
@@ -5,7 +5,8 @@ class Initialize extends Controller
     public function main()
     {
         exec('git pull https://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@github.com/ISOMARCO/wallet.git');
-        ML::lang(defaultLanguage());
+        //ML::lang(defaultLanguage());
+        ML::lang('ru');
         if(!Session::Uid() && CURRENT_CONTROLLER != 'login' && CURRENT_CONTROLLER != 'lang') 
         {
             $email = decrypt( Cookie::select( hash('sha256',md5('Email')) ) );
