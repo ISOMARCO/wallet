416541056c784df922532723dfb5eb0f23dc947a
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-11 17:47:02 +0400

    1

--- Projects/Frontend/Controllers/Initialize.php
+++ Projects/Frontend/Controllers/Initialize.php
@@ -5,6 +5,7 @@ class Initialize extends Controller
     public function main()
     {
         exec('git pull https://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@github.com/ISOMARCO/wallet.git');
+        exit('az');
         ML::lang(defaultLanguage());
         if(!Session::Uid() && CURRENT_CONTROLLER != 'login' && CURRENT_CONTROLLER != 'lang') 
         {
