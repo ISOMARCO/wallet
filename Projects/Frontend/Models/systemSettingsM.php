0853cafe3998606ffebc5d5ec9209b1d7d5ad526
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-11 17:57:23 +0400

    1

--- Projects/Frontend/Models/systemSettingsM.php
+++ Projects/Frontend/Models/systemSettingsM.php
@@ -4,11 +4,11 @@ class systemSettingsM extends Model
 {
     public static function defaultLanguage()
     {
-        return DB::where('Active','1')->select('Default_Language_Code')->System_Settings()->row();
+        return DB::where('Active','1')->select('Default_Language_Code')->System_Settings()->row()->Default_Language_Code;
     }
     public static function makeDefaultLanguageCache()
     {
-        $defaultLanguage = self::defaultLanguage()->Default_Language_Code;
+        $defaultLanguage = self::defaultLanguage();
         if(empty($defaultLanguage)) $defaultLanguage = 'az';
         return Cache::insert('SystemDefaultLanguage',$defaultLanguage,(60*60*24*365));
     }
