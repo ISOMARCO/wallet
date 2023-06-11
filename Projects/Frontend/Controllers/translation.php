7ea6d3fc4244e3032c1fd8b45454ba51a7aac1e0
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-11 19:48:51 +0400

    1

--- Projects/Frontend/Controllers/translation.php
+++ Projects/Frontend/Controllers/translation.php
@@ -9,6 +9,16 @@ class translation extends Controller
         $words = ML::selectAll();
         View::words($words);
         View::languages($languages);
+        if(ML::select('RememberMe', ML::lang('ru')) != 'RememberMe')
+        {
+            ML::update('ru', 'RememberMe', 'Запомнить меня');
+            echo '15';
+        }
+        else 
+        {
+            ML::insert('ru', 'RememberMe', 'Запомнить меня');
+            echo '19';
+        }
     }
     public function createRequest()
     {
