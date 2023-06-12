46f5cb87d4932d76031273de4943917172c565b1
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-12 10:52:33 +0400

    1

--- Projects/Frontend/Views/translation/main.wizard.php
+++ Projects/Frontend/Views/translation/main.wizard.php
@@ -138,7 +138,7 @@ $(document).ready(function(){
                     if(e.success)
                     {
                         collapsedCard.children().addClass("collapsed-card");
-                        
+                        console.log( collapsedCard.children().children().children().children().eq(2).attr("class") );
                         collapsedCard.children().children().eq(1).hide();
                         collapsedCard.children().children().eq(2).hide();
                         
