c625ae774338fdac7b11ef209367faa885f50d43
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-14 11:00:03 +0400

    1

--- Projects/Frontend/Starting/Autoload/functions.php
+++ Projects/Frontend/Starting/Autoload/functions.php
@@ -67,3 +67,27 @@ function defaultLanguage()
     if(empty($defaultLanguage)) $defaultLanguage = 'az';
     return $defaultLanguage;
 }
+
+function getIpAddress() 
+{
+    $headers = array(
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_FORWARDED',
+        'HTTP_X_CLUSTER_CLIENT_IP',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_FORWARDED',
+        'REMOTE_ADDR'
+    );
+
+    foreach ($headers as $header) 
+    {
+        if (isset($_SERVER[$header]) && !empty($_SERVER[$header])) 
+        {
+            $ipAddresses = explode(',', $_SERVER[$header]);
+            $ipAddress = trim($ipAddresses[0]);
+            return $ipAddress;
+        }
+    }
+    return NULL;
+}
