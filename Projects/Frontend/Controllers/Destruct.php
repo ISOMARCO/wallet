efce4743136b36def82f6adabaa96c473ca31ea8
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-14 14:02:08 +0400

    1

--- Projects/Frontend/Controllers/Destruct.php
+++ Projects/Frontend/Controllers/Destruct.php
@@ -4,6 +4,8 @@ class Destruct extends Controller
 {
     public function main()
     {
+        echo Session::Uid()." ".loginM::checkLogout();
+        exit;
         if( Session::Uid() && loginM::checkLogout() && CURRENT_CONTROLLER != 'login' )
         {
             loginM::logout();
