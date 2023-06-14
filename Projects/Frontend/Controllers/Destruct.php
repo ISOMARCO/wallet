3dffc591112563269b9819b2d374a31e2ce6823f
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-14 14:06:34 +0400

    1

--- Projects/Frontend/Controllers/Destruct.php
+++ Projects/Frontend/Controllers/Destruct.php
@@ -4,8 +4,7 @@ class Destruct extends Controller
 {
     public function main()
     {
-        #echo DB::select('Id')->where('User', Session::Uid())->where('User_Agent', $_SERVER['HTTP_USER_AGENT'])->totalRows();
-        echo Session::Uid()." sds";
+        echo DB::select('Id')->where('User', Session::Uid())->where('User_Agent', $_SERVER['HTTP_USER_AGENT'])->stringQuery();
         exit;
         if( Session::Uid() && loginM::checkLogout() && CURRENT_CONTROLLER != 'login' )
         {
