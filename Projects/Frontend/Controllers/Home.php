3090760041382f41b7de21e0c7cd22b250bb3ac8
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-14 17:40:35 +0400

    1

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -5,7 +5,6 @@ class Home extends Controller
     public function main(string ...$parameters)
     {
         Masterpage::title('Home');
-        echo detectBrowser('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43');
     } 
     public function exit($all = NULL)
     {
