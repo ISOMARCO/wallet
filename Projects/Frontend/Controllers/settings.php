b7f13a0d9c0dc0175b07bf0768d99c7734f4fc3d
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-17 14:45:45 +0400

    1

--- Projects/Frontend/Controllers/settings.php
+++ Projects/Frontend/Controllers/settings.php
@@ -1,15 +1,15 @@
 <?php namespace Project\Controllers;
-use ML, settingsM;
+use ML, settingsM, BrowserDetection;
 class settings extends Controller
 {
     public function main()
     {
         Masterpage::title(ML::select('Settings'));
+        output(BrowserDetection::getBrowser($_SERVER['HTTP_USER_AGENT']));
     }
     public function Logged_Devices()
     {
         Masterpage::title(ML::select('LoggedDevices'));
         View::loggedDevices(settingsM::loggedDevices());
-        output(settingsM::loggedDevices());
     }
 }
--- Projects/Frontend/Views/settings/Logged_Devices.wizard.php
+++ Projects/Frontend/Views/settings/Logged_Devices.wizard.php
@@ -20,35 +20,37 @@
 
 </div>
 <div class="card-body">
-    <div class="card collapsed-card">
-        <div class="card-header">
-            <div class="row">
-                <div class="col-1 col-sm-1">
-                    <a href="javascript:void(0)"><i class="fas fa-arrow-circle-right fa-lg"></i></a>
-                </div>
-                <div class="col-10 col-sm-10">
-                    <h3 class="card-title font-weight-bold">
-                        Name
-                    </h3>
-                </div>  
-                <div class="card-tools col-1 col-sm-1">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-plus"></i>
-                    </button>
+    @foreach($loggedDevices as $value)
+        <div class="card collapsed-card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-1 col-sm-1">
+                        <a href="javascript:void(0)"><i class="fas fa-arrow-circle-right fa-lg"></i></a>
+                    </div>
+                    <div class="col-10 col-sm-10">
+                        <h3 class="card-title font-weight-bold">
+                            {{BrowserDetection::getBrowser($value->User_Agent)['Name']}}
+                        </h3>
+                    </div>  
+                    <div class="card-tools col-1 col-sm-1">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                        <i class="fas fa-plus"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="card-body">
+            <div class="card-body">
+                
             
-        
 
 
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-primary" style="float:left">Edit&nbsp;<i class="fas fa-edit"></i></button>
+                <button type="button" class="btn btn-danger" style="float:right" onclick="return confirm('Are you sure?')">Delete&nbsp;<i class="fas fa-trash"></i></button>
+            </div>
         </div>
-        <div class="card-footer">
-            <button type="button" class="btn btn-primary" style="float:left">Edit&nbsp;<i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger" style="float:right" onclick="return confirm('Are you sure?')">Delete&nbsp;<i class="fas fa-trash"></i></button>
-        </div>
-    </div>
+    @endforeach
 </div>
 </section>
 </div>
