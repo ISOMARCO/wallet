73525825f115cebbae4dc34db87354ef481ba44a
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-17 15:01:04 +0400

    1

--- Projects/Frontend/Views/settings/Logged_Devices.wizard.php
+++ Projects/Frontend/Views/settings/Logged_Devices.wizard.php
@@ -41,7 +41,7 @@
             </div>
             <div class="card-body">
                 Ip Address: {{$value->Ip_Address}}<br>
-                Browser: {{BrowserDetection::getBrowser($value->User_Agent)['browser_name']}}
+                Browser: {{strtoupper(BrowserDetection::getBrowser($value->User_Agent)['browser_name'])}}
             </div>
             <div class="card-footer">
                 <button type="button" class="btn btn-danger" style="float:right" onclick="return confirm('Are you sure?')">{{ML::select('Exit')}}&nbsp;<i class="fas fa-sign-out-alt"></i></button>
