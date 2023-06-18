698a9f12417e7207c8ed49eb797117bbb67ef281
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-18 12:19:50 +0400

    1

--- Projects/Frontend/Views/Sections/footer.wizard.php
+++ Projects/Frontend/Views/Sections/footer.wizard.php
@@ -14,6 +14,7 @@
 <!-- ./wrapper -->
 
 <!-- Bootstrap 4 -->
+<script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
