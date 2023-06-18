1433eee67d19f583906fa96897bb0e20ac71625e
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-18 12:26:22 +0400

    1

--- Projects/Frontend/Views/Sections/footer.wizard.php
+++ Projects/Frontend/Views/Sections/footer.wizard.php
@@ -17,8 +17,6 @@
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
-<!-- Socket App -->
-<script src="dist/js/app.js"></script>
 <!-- select2 -->
 <script src="plugins/select2/js/select2.min.js"></script>
 <!-- sweet alert2 -->
