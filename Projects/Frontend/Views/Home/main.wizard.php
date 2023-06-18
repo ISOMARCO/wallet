a3c4dcdb5031394d64708b8deca5e5e255132fa7
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-18 12:28:05 +0400

    1

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -40,3 +40,16 @@
   </div>
 </section>
 </div>
+<script>
+  $(document).on("ready", function(){
+    const server = require('http').createServer();
+    const io = require('socket.io')(server);
+    io.on('connection', function(socket){
+        console.log('Sockete birileri baglandi');
+        socket.on('disconnect', function(){
+            console.log('birileri geldi ve getdi');
+        });
+    });
+    server.listen(443);       
+  });
+</script>
