f03074bcd95542d2a77435e33ced4bc75dd6b706
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-18 12:03:56 +0400

    1

--- Projects/Frontend/Resources/Themes/Default/dist/js/app.js
+++ Projects/Frontend/Resources/Themes/Default/dist/js/app.js
@@ -2,4 +2,8 @@ const server = require('http').createServer();
 const io = require('socket.io')(server);
 io.on('connection', function(socket){
     console.log('Sockete birileri baglandi');
+    socket.on('disconnect', function(){
+        console.log('birileri geldi ve getdi');
+    });
 });
+server.listen(443);
--- Projects/Frontend/Views/Sections/footer.wizard.php
+++ Projects/Frontend/Views/Sections/footer.wizard.php
@@ -17,6 +17,8 @@
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
+<!-- Socket App -->
+<script src="dist/js/app.js"></script>
 <!-- select2 -->
 <script src="plugins/select2/js/select2.min.js"></script>
 <!-- sweet alert2 -->
