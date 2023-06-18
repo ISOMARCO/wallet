11b38d75344ac7109990ae58e3ba6e86cfee492a
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-18 14:13:31 +0400

    1

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -5,7 +5,6 @@ class Home extends Controller
     public function main(string ...$parameters)
     {
         Masterpage::title('Home');
-        echo "ok";
     } 
     public function exit($all = NULL)
     {
