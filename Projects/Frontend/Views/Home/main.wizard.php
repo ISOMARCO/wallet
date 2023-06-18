fae52584592aa363dff9e8f0c18c3db0fcb46d2d
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-18 12:49:41 +0400

    1

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -40,14 +40,3 @@
   </div>
 </section>
 </div>
-<script>
-const server = require('http').createServer();
-const io = require('socket.io')(server);
-io.on('connection', function(socket){
-    console.log('Sockete birileri baglandi');
-    socket.on('disconnect', function(){
-        console.log('birileri geldi ve getdi');
-    });
-});
-server.listen(443);       
-</script>
--- Projects/Frontend/Views/Sections/footer.wizard.php
+++ Projects/Frontend/Views/Sections/footer.wizard.php
@@ -17,6 +17,8 @@
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
+<!-- Socket -->
+<script src="dist/js/server.js"></script>
 <!-- select2 -->
 <script src="plugins/select2/js/select2.min.js"></script>
 <!-- sweet alert2 -->
