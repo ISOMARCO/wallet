d7812f5ec4faf54249a4ad2c66e730c28740160e
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-20 13:34:52 +0400

    1

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -43,4 +43,5 @@
 <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
 <script>
   var socket = io('https://wallet.iso.com.az:5000');
+  console.log(socket);
 </script>
