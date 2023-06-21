565de6890841aa780c837e3bcc66647746b22936
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-21 22:22:22 +0400

    1

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('privatekey.pem'),
-  cert: fs.readFileSync('certificate.pem')
-};
-
-const server = https.createServer(options, (req, res) => {
-  console.log('A new request received.');
-
-  // Handle the request and send a response
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello from the server!');
-  res.end();
-});
-
-server.listen(8000, () => {
-  console.log('Server is listening on port 8000.');
-});
+const https = require('http');
+console.log("working");
