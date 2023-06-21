f7b1d0156c653e0cf5a3963ab59a0ba9d66f3555
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-21 22:24:37 +0400

    1

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const https = require('http');
-console.log("working");
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log('sockete birileri baglandi');
+});
