28019491c6f228b6472d321007d137779567bdfe
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-24 01:30:14 +0400

    1

--- Projects/Frontend/Controllers/Webhook.php
+++ Projects/Frontend/Controllers/Webhook.php
@@ -1,9 +1,10 @@
 <?php namespace Project\Controllers;
-use TelegramBot;
+use TelegramBot, URL;
 class Webhook extends Controller
 {
     public function main()
     {
+        TelegramBot::setWebhook(URL::base('Webhook'));
         $data = TelegramBot::getData();
         if($data->text == 'hello')
         {
