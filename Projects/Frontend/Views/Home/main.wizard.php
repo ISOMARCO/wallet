62f22cb5c0c987e7c3a6160e1a2b800ff645fe81
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-23 14:11:21 +0400

    1

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -42,6 +42,6 @@
 </div>
 <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
 <script>
-  var socket = io('http://194.163.181.227:2083');
+  var socket = io('http://194.163.181.227:2052');
   console.log(socket);
 </script>
--- app.js
+++ app.js
@@ -5,4 +5,4 @@ io.on('connection', function(socket){
   console.log('sockete birileri baglandi');
 });
 
-server.listen(2083);
+server.listen(2052);
