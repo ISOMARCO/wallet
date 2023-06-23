1c59e6901fdf294fe041061fa45b9a26a1ed7e6b
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-23 13:55:42 +0400

    1

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
-var connection = new io.connect('194.163.181.227:2083', {
-    'reconnection': true,
-    'reconnectionDelay': 1000,
-    'reconnectionDelayMax' : 5000,
-    'reconnectionAttempts': 5
+io.on('connection', function(socket){
+  console.log('sockete birileri baglandi');
 });
-console.log(connection);
+
 server.listen(2083);
