ddd792f738150a9fd753df156d445418b3037164
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-24 00:26:38 +0400

    1

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends Controller
     public function main(string ...$parameters)
     {
         Masterpage::title('Home');
-        echo TelegramBot::setToken('5534810537:AAEfTCYFMsg1qmPsxk3t6Rmnc3Jp8yqurD0')->showUrl;
+        echo TelegramBot::setToken('5534810537:AAEfTCYFMsg1qmPsxk3t6Rmnc3Jp8yqurD0')->showUrl();
     } 
     public function exit($all = NULL)
     {
