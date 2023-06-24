d43341f0d9e932938a5f2683e8f2b04dd0c81cb8
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-24 15:48:24 +0400

    1

--- webhook_demo.php
+++ webhook_demo.php
@@ -136,7 +136,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
-    return 'Error:' . curl_error($ch);
+    echo 'Error:' . curl_error($ch);
 }
 curl_close($ch);
 echo json_encode($result);
