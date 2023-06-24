e918fda8fa460d496ffea0e021543dbbfa938f71
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-24 15:50:54 +0400

    1

--- webhook_demo.php
+++ webhook_demo.php
@@ -1,6 +1,6 @@
 <?php 
 
-/*class TelegramBot 
+class TelegramBot 
 {
     const API_URL = 'https://api.telegram.org/bot';
     public $token = '5534810537:AAEfTCYFMsg1qmPsxk3t6Rmnc3Jp8yqurD0';
@@ -122,21 +122,4 @@ else
         'resize_keyboard' => true
     );
     $telegram->sendMessage('demo', json_encode($markup));
-}*/
-$ch = curl_init();
-$url = 'https://api.telegram.org/bot5534810537:AAEfTCYFMsg1qmPsxk3t6Rmnc3Jp8yqurD0/getMe';
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($posts));
-
-$headers = array();
-$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
 }
-curl_close($ch);
-print_r($result);
