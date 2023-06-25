5fccaee2d8f11933f93d6a7a9833497b39487aee
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-25 17:19:43 +0400

    1

--- Projects/Frontend/Libraries/TelegramBot.php
+++ Projects/Frontend/Libraries/TelegramBot.php
@@ -64,7 +64,7 @@ class InternalTelegramBot
 
     public function getData()
     {
-        $data = json_decode( file_get_contents("php://input") );
+        $data = json_decode( file_get_contents("php://input"), true );
         $this->chatId = $data->message->chat->id;
         DB::insert("Logs", ["Text" => $data]);
         return $data;
