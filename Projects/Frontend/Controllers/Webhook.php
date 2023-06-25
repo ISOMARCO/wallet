396b7cbcd9e2ea087e97db3e7e60f45d698a8fe2
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-25 16:43:32 +0400

    1

--- Projects/Frontend/Controllers/Webhook.php
+++ Projects/Frontend/Controllers/Webhook.php
@@ -1,5 +1,5 @@
 <?php namespace Project\Controllers;
-use TelegramBot, URL, DB;
+use TelegramBot, URL, DB, ML;
 class Webhook extends Controller
 {
     public function main()
@@ -18,19 +18,16 @@ class Webhook extends Controller
                     ]
                 ]
             ];
-            TelegramBot::sendMessage('Profilinizi paylaşmak için aşağıdaki düğmeye basın:',json_encode($keyboard));
-            #$dataArray = json_decode($data);
+            $markup = array(
+                'keyboard' => $keyboard,
+                'text' => 'menu_demo',
+                'resize_keyboard' => true
+            );
+            TelegramBot::sendMessage('Profilinizi paylaşmak için aşağıdaki düğmeye basın:',json_encode($markup));
             DB::insert("Logs", [
                 "Text" => $data,
                 "Username" => $data->message->chat->username,
                 "ChatId" => $data->message->chat->id
-            ]);
-        }
-        if(strtolower($data->message->text) == 'hello')
-        {
-            TelegramBot::sendMessage("Hello2");
-            DB::insert("Logs", [
-                "Text" => $data
             ]);
         }
     }
