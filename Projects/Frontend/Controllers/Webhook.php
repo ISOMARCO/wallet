45d8486809f30551b7d6276d309c333663e86dc1
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-25 16:28:55 +0400

    1

--- Projects/Frontend/Controllers/Webhook.php
+++ Projects/Frontend/Controllers/Webhook.php
@@ -21,9 +21,9 @@ class Webhook extends Controller
             TelegramBot::sendMessage('Profilinizi paylaşmak için aşağıdaki düğmeye basın:',json_encode($keyboard));
             $dataArray = json_decode($data, true);
             DB::insert("Logs", [
-                "Text" => $data,
-                "Username" => $dataArray['message']['chat']['username'],
-                "ChatId" => $dataArray['message']['chat']['id']
+                "Text" => $data//,
+                //"Username" => $dataArray['message']['chat']['username'],
+                //"ChatId" => $dataArray['message']['chat']['id']
             ]);
         }
         if(strtolower($data->message->text) == 'hello')
