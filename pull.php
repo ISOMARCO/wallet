22d51816b5ed1475ed34e9424d6d97178b31556a
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-25 10:10:19 +0400

    1

--- pull.php
+++ pull.php
@@ -2,3 +2,4 @@
 include 'Projects/Frontend/Libraries/Demo.php';
 print_r(exec('git pull https://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@github.com/ISOMARCO/wallet.git'));
 $demo = new Demo();
+echo $demo->demo1();
