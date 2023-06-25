c1d9f90e32e67548fbde5517e5c1e56ab8b3e168
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-25 10:10:00 +0400

    1

---
+++ Projects/Frontend/Libraries/Demo.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Demo 
+{
+    public function demo1()
+    {
+        return 'okey';
+    }
+}
--- pull.php
+++ pull.php
@@ -1,4 +1,4 @@
 <?php 
-include 'Internal/autoload.php';
-echo URL::base('home');
+include 'Projects/Frontend/Libraries/Demo.php';
 print_r(exec('git pull https://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@github.com/ISOMARCO/wallet.git'));
+$demo = new Demo();
