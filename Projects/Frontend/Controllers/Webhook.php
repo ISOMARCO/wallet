6c6fb0053b69cfc604272bb5aab7e7ccdf7770ad
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-25 12:21:29 +0400

    1

--- Projects/Frontend/Controllers/Webhook.php
+++ Projects/Frontend/Controllers/Webhook.php
@@ -1,10 +1,9 @@
 <?php namespace Project\Controllers;
-use TelegramBot, URL, Redirect;
+use TelegramBot, URL;
 class Webhook extends Controller
 {
     public function main()
     {
-        #Redirect::code(302)->action('https://wallet.iso.com.az/webhook_demo.php');
         TelegramBot::sendMessage('demo');
         $data = TelegramBot::getData();
         if(strtolower($data->text) == 'hello')
--- Projects/Frontend/Routes/Routes.php
+++ Projects/Frontend/Routes/Routes.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::show404('Home/s404');
-Route::change('webhook_demo.php')->uri('home');
-#Route::method('post')->uri('Webhook');
+#Route::change('webhook_demo.php')->uri('home');
+Route::direct()->method('post')->uri('Webhook');
