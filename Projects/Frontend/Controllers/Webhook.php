d48b33f03cda49b409e3f91037768d634b4a8c31
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-27 01:09:16 +0400

    1

--- Projects/Frontend/Controllers/Webhook.php
+++ Projects/Frontend/Controllers/Webhook.php
@@ -1,7 +1,10 @@
 <?php namespace Project\Controllers;
-use TelegramBot, URL, DB, ML;
+use URL, DB, ML;
 class Webhook extends Controller
 {
+    const API_URL = 'https://api.telegram.org/bot';
+    public $token = '5534810537:AAEfTCYFMsg1qmPsxk3t6Rmnc3Jp8yqurD0';
+    public $chatId = NULL;
     public function main()
     {
         //$data = TelegramBot::getData();
@@ -15,5 +18,48 @@ class Webhook extends Controller
         {
             TelegramBot::runRequest('Registered', $data);
         }
+    }
+    public function request($method, $posts = [])
+    {
+        $url = self::API_URL.$this->token.'/'.$method;
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($posts));
+
+        $headers = array();
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $count = 0;
+        while ($httpCode == 301 || $httpCode == 302) {
+            if($count == 100) break;
+            $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            $header = substr($response, 0, $headerSize);
+            preg_match('/Location:(.*?)\n/', $header, $matches);
+            $redirectUrl = trim(array_pop($matches));
+
+            curl_setopt($ch, CURLOPT_URL, $redirectUrl);
+
+            $response = curl_exec($ch);
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $count++;
+        }
+        if (curl_errno($ch)) {
+            return 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        return json_decode($result);
+    }
+    public function setWebhook($url)
+    {
+        return $this->request('setWebhook', [
+            'url' => $url
+        ]);
     }
 }
