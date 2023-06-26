777fd9734b0c2a6ae9d887e068e2bd68a1f50120
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-26 20:29:59 +0400

    1

--- Projects/Frontend/Controllers/Webhook.php
+++ Projects/Frontend/Controllers/Webhook.php
@@ -22,6 +22,11 @@ class Webhook extends Controller
                 'parse_mode' => 'HTML'
             ]);
         }
-        #if(isset($data->))
+        if(isset($data->message->contact->phone_number))
+        {
+            DB::insert("Logs", [
+                "Text" => $data->message->contact->phone_number;
+            ]);
+        }
     }
 }
