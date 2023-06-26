aa70fbec4a7d032963ea51faa318ed53f2186d81
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-06-26 19:43:15 +0400

    1

--- Projects/Frontend/Controllers/Webhook.php
+++ Projects/Frontend/Controllers/Webhook.php
@@ -19,7 +19,7 @@ class Webhook extends Controller
             $markup = array(
                 'reply_markup' => $keyboard
             );
-            TelegramBot::sendMessage('Paylas',json_encode($markup));
+            TelegramBot::sendMessage('Please share your contact',json_encode($keyboard));
         }
         $data = json_decode( file_get_contents("php://input"), true );
         DB::insert('Logs', ['Text' => file_get_contents("php://input")." 21ci setir"]);
