4035bc1b054f31bd22c896ddeb8317bffd1f9f2d
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-07-01 11:09:29 +0400

    1

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -8,7 +8,7 @@ class Home extends Controller
         foreach(DB::Logs()->result() as $value)
         {
             #$data = json_decode($value->Text, true);
-            echo $value->Text."<br>";
+            #echo $value->Text."<br>";
         }
     } 
     public function exit($all = NULL)
