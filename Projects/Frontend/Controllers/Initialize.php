e77bec68c011b35f0092619330507b62d86f2f77
ISOMARCO <inagiyev@icloud.com>
2023-07-25 13:19:16 +0400

    1

--- Projects/Frontend/Controllers/Initialize.php
+++ Projects/Frontend/Controllers/Initialize.php
@@ -4,7 +4,7 @@ class Initialize extends Controller
 {
     public function main()
     {
-        exec('git pull https://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@github.com/ISOMARCO/wallet.git');
+        //exec('git pull https://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@github.com/ISOMARCO/wallet.git');
         $defaultLanguage = defaultLanguage();
         ML::lang($defaultLanguage);
         View::defaultLanguage($defaultLanguage);
